bca7b518997bc5cd41f73b79a9839345d8c5d011
suburbandox <emkline@gmail.com>
2020-12-11 15:14:50 -0600

    the flip

---
+++ hell2/Assets/script/player/projectlie.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectlie : MonoBehaviour
+{
+
+    public float sp;
+    public float lifetime;
+    public GameObject dedfx;
+    public float dis;
+    public LayerMask solid;
+    public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ded", lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.up, dis, solid);
+        if (ray.collider != null)
+        {
+            if (ray.collider.CompareTag("enemy"))
+            {
+                Debug.Log("uh o");
+                ray.collider.GetComponent<enemy>().takedamage(damage);
+            }
+            ded();
+        }
+        transform.Translate(Vector2.up * sp * Time.deltaTime);
+    }
+    void ded()
+    {
+        Instantiate(dedfx,transform.position,Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
