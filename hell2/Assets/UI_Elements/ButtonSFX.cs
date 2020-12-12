ca446058605e16f0ebe0eafd5541213b923a4d78
storiesstrauss <storiesstrauss@gmail.com>
2020-12-12 22:08:32 +0800

    Main Menu
    set up

---
+++ hell2/Assets/UI_Elements/ButtonSFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSFX : MonoBehaviour
+{
+    public AudioSource soundFX;
+    public AudioClip hoverSFX;
+    public AudioClip clickSFX;
+
+    public void HoverSound()
+    {
+        soundFX.PlayOneShot(hoverSFX);
+    }
+
+    public void ClickSound()
+    {
+        soundFX.PlayOneShot(clickSFX);
+    }
+}
---
+++ hell2/Assets/UI_Elements/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject optionsPopout;
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("hell");
+    }
+
+    public void openOptions()
+    {
+        optionsPopout.SetActive(true);
+    }
+
+    public void closeOptions()
+    {
+        optionsPopout.SetActive(false);
+    }
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+
+}
