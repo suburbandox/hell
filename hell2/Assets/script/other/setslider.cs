18d586630bc617343251c663bc335166b2cc1f34
suburbandox <emkline@gmail.com>
2020-12-12 16:09:05 -0600

    gun

---
+++ hell2/Assets/script/other/setslider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class setslider : MonoBehaviour
+{
+    public AudioMixer doom;
+    public Slider music;
+    public Slider sfx;
+    public Slider master;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //master.value = doom.GetFloat("master",) 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
