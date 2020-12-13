c3f16a9efbd49e0a3398d0377e3d0f3111e5e9b0
suburbandox <emkline@gmail.com>
2020-12-13 13:25:13 -0600

    new

---
+++ hell2/Assets/script/ren2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ren2 : MonoBehaviour
+{
+
+    public Transform p;
+    public SpriteRenderer s;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = GameObject.FindGameObjectWithTag("enemy").transform;
+        s = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (p.position.y > transform.position.y)
+        {
+            s.color = new Color(255, 255, 255, 0.5f);
+            s.sortingOrder = 100;
+        }
+        else
+        {
+            s.color = new Color(255, 255, 255, 1f);
+            s.sortingOrder = -100;
+        }
+    }
+
+
+}
